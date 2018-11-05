f32bc10d1c22ba362f3700d402271a6741a39d00
Joel Ruhe <>
2018-11-05 16:26:35 +0100

    nothing

--- App/AndroidStudioProjects/MyApplication/app/src/main/java/com/example/joelruhe/myapplication/activities/LoginActivity.java
+++ App/AndroidStudioProjects/MyApplication/app/src/main/java/com/example/joelruhe/myapplication/activities/LoginActivity.java
@@ -24,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        UsernameEt = (EditText)findViewById(R.id.textUsername);
+        UsernameEt = (EditText)findViewById(R.id.textEmail);
         PasswordEt = (EditText)findViewById(R.id.textPassword);
         Register = (TextView)findViewById((R.id.register));
 
